56ec062a2881d8a1a6b64faaa9b980792c420289
Marina Fedyantseva <marie.saez@mail.ru>
2026-01-19 13:08:39 +0300

    [AS-485] remove old captcha implementation

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,6 @@ import { History } from 'pages/History';
 import { useIdleTimer, useDeepLink } from 'hooks';
 import TransactionPollingService from 'services/transactionPolling';
 import FeedbackForm from 'components/community/FeedbackForm';
-import CaptchaModal from 'components/Captcha/modal/CaptchaModal';
 
 const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { isAuthenticated, hasAccounts } = useSelector((state: RootState) => state.auth);
@@ -150,7 +149,6 @@ const AppContent: React.FC = () => {
         } />
       </Routes>
       <FeedbackForm />
-      <CaptchaModal/>
     </ThemeProvider>
   );
 };
